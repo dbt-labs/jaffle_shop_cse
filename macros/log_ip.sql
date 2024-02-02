0b81474623f1b5a59e222402f92be032cb26e406
J Y <jeremy.yeo@dbtlabs.com>
2024-02-02 00:27:59 +0000

    update

--- macros/log_ip.sql
+++ macros/log_ip.sql
@@ -1,6 +1,6 @@
 {% macro log_source_ip() %}
     {% set v = run_query("select current_ip_address() as source_ip") %}
-    {% do log('>>>>>>> the source ip is: ' ~ v.columns[0].values()[0]), info=True %}
+    {% do log('>>>>>>> the source ip is: ' ~ v.columns[0].values()[0], info=True) %}
     {% set q -%}
         insert into development.dbt_jyeo.asb_ip_logger values (sysdate(),  current_ip_address());
     {%- endset %}
